d21a3d40500825b3029ef46e88adf88c8ec154e5
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-09-06 04:02:26 -0500

    1

--- module_test_for_manifest_description/__manifest__.py
+++ module_test_for_manifest_description/__manifest__.py
@@ -21,7 +21,7 @@ Step 2: Switch to company related to that GST number
 Step 3: Set that username and password in Odoo (Goto: Invoicing/Accounting -> Configuration -> Settings -> Customer Invoices or find "E-invoice" in search bar)
 Step 4: Repeat steps 1,2,3 for all GSTIN you have in odoo. If you have a multi-company with the same GST number then perform step 1 for the first company only.
 
-For the creation of API username and password please ref this document: (`reference </apps/modules/16.0/to_attendance_device>`_)
+For the creation of API username and password please ref this document: (`reference <https://www.youtube.com/watch?v=bNbP4O9b_Ak>`_)
     """,
     "data": [
         "data/account_edi_data.xml",
